12ad4efcaa7d9df53aa0762ec5e256b9806659c8
Dele Olajide <dele@4ng.net>
2018-05-16 23:46:42 +0100

    No more tab window opens for ofmeet.

--- webmeet-component/jitsimeet/ofmeet.js
+++ webmeet-component/jitsimeet/ofmeet.js
@@ -4,9 +4,20 @@
 
 var ofmeet = (function(of)
 {
-    var firstTime = true;
-    var firstTrack = true;
-    var participants = {}
+    var room = urlParam("room");
+
+    var recordAudio = JSON.parse(window.localStorage["webmeet_record_audio"]);
+    var recordVideo = JSON.parse(window.localStorage["webmeet_record_video"]);
+    var enableTranscription = JSON.parse(window.localStorage["webmeet_transcription"]);
+    var enableCaptions = JSON.parse(window.localStorage["webmeet_captions"]);
+    var enableRecord = JSON.parse(window.localStorage["webmeet_record"]);
+
+    function urlParam(name)
+    {
+        var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href);
+        if (!results) { return undefined; }
+        return unescape(results[1] || undefined);
+    };
 
     function setup()
     {
@@ -30,18 +41,6 @@ var ofmeet = (function(of)
             $(message).find('ofmeet').each(function ()
             {
                 try {
-                    var json = JSON.parse($(this).text());
-
-                    if (json.event == "ofmeet.event.sip.join")
-                    {
-                        addSipParticipant(json);
-                    }
-                    else
-
-                    if (json.event == "ofmeet.event.sip.leave")
-                    {
-                        removeSipParticipant(json);
-                    }
 
 
                 } catch (e) {}
@@ -66,7 +65,10 @@ var ofmeet = (function(of)
     {
         console.log("ofmeet.js __init");
 
-        //of.subtitles = document.getElementById("subtitles");
+        of.username = localStorage.getItem("xmpp_username_override").split("@")[0];
+        of.password = localStorage.getItem("xmpp_password_override");
+
+        of.subtitles = document.getElementById("subtitles");
 
         APP.conference.addConferenceListener(JitsiMeetJS.events.conference.CONFERENCE_JOINED, function()
         {
@@ -77,22 +79,23 @@ var ofmeet = (function(of)
         {
             console.log("ofmeet.js me left");
 
-
             if (of.recognition)
             {
                 of.recognitionActive = false;
                 of.recognition.stop();
             }
+
+            if (parent && parent.stopRecorder) parent.stopRecorder();
         });
 
         APP.conference.addConferenceListener(JitsiMeetJS.events.conference.MESSAGE_RECEIVED , function(id, text, ts)
         {
             console.log("ofmeet.js message", id, text, ts);
 
-            //if (OFMEET_CONFIG.enableCaptions)
-            //{
-            //    of.subtitles.innerHTML = id.split("-")[0] + " : " + text;
-            //}
+            if (enableCaptions && text.indexOf("http") != 0)
+            {
+                of.subtitles.innerHTML = id.split("-")[0] + " : " + text;
+            }
         });
 
         APP.conference.addConferenceListener(JitsiMeetJS.events.conference.USER_LEFT, function(id)
@@ -123,25 +126,38 @@ var ofmeet = (function(of)
         APP.conference.addConferenceListener(JitsiMeetJS.events.conference.TRACK_MUTE_CHANGED, function(track)
         {
             console.log("ofmeet.js track muted", track.getParticipantId(), track.getType(), track.isMuted());
-
-            if (APP.conference.getMyUserId() == track.getParticipantId() && of.localStream)
-            {
-                var tracks = of.localStream.getAudioTracks();
-
-                for (var i=0; i<tracks.length; i++)
-                {
-                    tracks[i].enabled = !track.isMuted();
-                }
-            }
         });
 
         if (APP.conference.roomName)
         {
-            //if (OFMEET_CONFIG.enableTranscription)
-            //{
+            if (of.username && of.password && enableRecord && parent && parent.startRecorder)
+            {
+                parent.startRecorder(recordAudio, recordVideo, APP.conference.localAudio.stream, APP.conference.localVideo.stream, room, APP.conference.getMyUserId(), of.username, of.password);
+
+                var audioFileName = room + "." + APP.conference.getMyUserId() + ".audio.webm";
+                var videoFileName = room + "." + APP.conference.getMyUserId() + ".video.webm";
+
+                var audioFileUrl = "https://" + location.host + "/ofmeet-cdn/recordings/" + audioFileName;
+                var videoFileUrl = "https://" + location.host + "/ofmeet-cdn/recordings/" + videoFileName;
+
+                if (recordAudio)
+                {
+                    APP.conference._room.sendTextMessage(audioFileUrl);
+                }
+                else
+
+                if (recordVideo)
+                {
+                    APP.conference._room.sendTextMessage(audioFileUrl);
+                    APP.conference._room.sendTextMessage(videoFileUrl);
+                }
+            }
+
+            if (enableTranscription)
+            {
                 setupSpeechRecognition();
                 of.recognition.start();
-            //}
+            }
         }
     }
 
@@ -200,9 +216,9 @@ var ofmeet = (function(of)
         if (result != "" && APP.conference && APP.conference._room)
         {
             var message = "[" + result + "]";
-            console.log("Speech recog result", APP.conference._room, message,  localStorage.getItem("xmpp_username_override"), localStorage.getItem("uport.name"));
+            console.log("Speech recog result", APP.conference._room, message);
 
-            APP.conference._room.sendTextMessage(message, localStorage.getItem("uport.name"));
+            APP.conference._room.sendTextMessage(message);
             of.currentTranslation = [];
         }
     }
@@ -210,12 +226,19 @@ var ofmeet = (function(of)
     window.addEventListener("beforeunload", function(e)
     {
         console.log("ofmeet.js beforeunload");
+        //e.returnValue = 'Ok';
+    });
 
-        localStorage.removeItem("xmpp_username_override");
-        localStorage.removeItem("xmpp_password_override");
-        localStorage.removeItem("uport.email");
-        localStorage.removeItem("uport.name");
-        localStorage.removeItem("uport.avatar");           
+    window.addEventListener("unload", function (e)
+    {
+        console.log("ofmeet.js unload");
+
+        //localStorage.removeItem("xmpp_username_override");
+        //localStorage.removeItem("xmpp_password_override");
+
+        //localStorage.removeItem("uport.email");
+        //localStorage.removeItem("uport.name");
+        //localStorage.removeItem("uport.avatar");
 
         APP.conference._room.leave();
 
@@ -224,19 +247,7 @@ var ofmeet = (function(of)
             of.connection.disconnect();
         }
 
-        //e.returnValue = 'Ok';
-    });
-
-    window.addEventListener("unload", function (e)
-    {
-        console.log("ofmeet.js unload");
-
-        localStorage.removeItem("xmpp_username_override");
-        localStorage.removeItem("xmpp_password_override");
-        localStorage.removeItem("uport.email");
-        localStorage.removeItem("uport.name");
-        localStorage.removeItem("uport.avatar");        
-        
+        //stopRecorder();
     });
 
     window.addEventListener("load", function()
@@ -246,10 +257,10 @@ var ofmeet = (function(of)
         if (localStorage.getItem("uport.email"))    APP.conference.changeLocalEmail(localStorage.getItem("uport.email"));
         if (localStorage.getItem("uport.name"))     APP.conference.changeLocalDisplayName(localStorage.getItem("uport.name"));
         if (localStorage.getItem("uport.avatar"))   APP.conference.changeLocalAvatarUrl(localStorage.getItem("uport.avatar"));
-            
+
         setTimeout(function() {setup();}, 1000);
     });
-            
+
     config.p2p = {
         enabled: true,
         stunServers: [
--- webmeet-component/webmeet/dist/webmeet.js
+++ webmeet-component/webmeet/dist/webmeet.js
@@ -1,5 +1,13 @@
+var of = {}
+
 window.addEventListener("load", function()
 {
+    window.localStorage["webmeet_record"]       =  config.webmeet_record;
+    window.localStorage["webmeet_record_audio"] =  config.webmeet_record_audio;
+    window.localStorage["webmeet_record_video"] =  config.webmeet_record_video;
+    window.localStorage["webmeet_transcription"] = config.webmeet_transcription;
+    window.localStorage["webmeet_captions"]     =  config.webmeet_captions;
+
     if (config.uport)
     {
         var clientId = "2p1psGHt9J5NBdPDQejSVhpsECXLxLaVQSo";
@@ -16,7 +24,7 @@ window.addEventListener("load", function()
 
             window.uport = new uportconnect.Connect("Pade", {clientId: clientId, signer: uportconnect.SimpleSigner(signer)});
 
-            window.uport.requestCredentials({notifications: true, verified: ['registration'], requested: ['name', 'email', 'phone', 'country', 'avatar']}).then((credentials) => 
+            window.uport.requestCredentials({notifications: true, verified: ['registration'], requested: ['name', 'email', 'phone', 'country', 'avatar']}).then((credentials) =>
             {
                 //console.log("Credentials", credentials);
 
@@ -28,18 +36,18 @@ window.addEventListener("load", function()
                 {
                     config.auto_join_rooms[0] = {jid: config.auto_join_rooms[0], nick: credentials.name};
                 }
-                
+
                 window.localStorage["uport.address"] =  credentials.address;
                 window.localStorage["uport.email"] =    credentials.email;
                 window.localStorage["uport.phone"] =    credentials.phone;
                 window.localStorage["uport.country"] =  credentials.country;
-                window.localStorage["uport.name"]    =  credentials.name;      
-                
+                window.localStorage["uport.name"]    =  credentials.name;
+
                 if (credentials.avatar && credentials.avatar.uri)
                 {
                     avatar = credentials.avatar.uri;
                     window.localStorage["uport.avatar"] = avatar;
-                }                
+                }
 
                 if (credentials.registration && credentials.registration.xmpp)
                 {
@@ -50,21 +58,21 @@ window.addEventListener("load", function()
                         config.authentication = "login";
                         config.jid = credentials.registration.xmpp;
                         config.password = credentials.registration.access;
-                        
+
                         // for jitsi meet
-                        
+
                         localStorage.setItem("xmpp_username_override", credentials.registration.xmpp);
-                        localStorage.setItem("xmpp_password_override", credentials.registration.access);                        
+                        localStorage.setItem("xmpp_password_override", credentials.registration.access);
 
                         document.getElementById("loader").style.display = "none";
                         converse.initialize(config);
-                        
+
                     } else {
-                        console.error("uPort credentials are not for server " + server + ", found " + credentials.registration.xmpp); 
+                        console.error("uPort credentials are not for server " + server + ", found " + credentials.registration.xmpp);
                         document.getElementById("loader").style.display = "none";
-                        converse.initialize(config);                        
+                        converse.initialize(config);
                     }
-                    
+
 
                 } else {
 
@@ -73,22 +81,22 @@ window.addEventListener("load", function()
 
                     web3.eth.getAccounts((error, result) =>
                     {
-                        console.log("account", account, error);
+                        console.log("account", result, error);
 
                         if (error) {
                             console.error('Account error', error);
                             document.getElementById("loader").style.display = "none";
-                            converse.initialize(config);     
+                            converse.initialize(config);
 
                         } else {
                             account = result[0];
-                            
-                            window.localStorage["uport.account"] =  account;  
-                                            
-                            console.log('Account:' + account);                
+
+                            window.localStorage["uport.account"] =  account;
+
+                            console.log('Account:' + account);
 
                             var url = "https://" + server + "/rest/api/restapi/v1/ask/uport/register";
-                            var options = {method: "POST", headers: {"authorization": permission}, body: JSON.stringify({name: credentials.name, email: credentials.email, phone: credentials.phone, country: credentials.country, address: credentials.address, publicKey: credentials.publicKey, avatar: avatar, password: "", account: account})};
+                            var options = {method: "POST", headers: {"authorization": permission}, body: JSON.stringify({name: credentials.name, email: credentials.email, phone: credentials.phone, country: credentials.country, address: credentials.address, publicKey: credentials.publicKey, avatar: credentials.avatar, password: "", account: account})};
 
                             //console.log("register new user", credentials);
 
@@ -110,12 +118,12 @@ window.addEventListener("load", function()
                                         config.authentication = "login";
                                         config.jid = userpass.username + "@" + config.locked_domain;
                                         config.password = userpass.password;
-                                        
+
                                         // for jitsi meet
 
                                         localStorage.setItem("xmpp_username_override", userpass.username + "@" + config.locked_domain);
-                                        localStorage.setItem("xmpp_password_override", userpass.password);                        
-                                        
+                                        localStorage.setItem("xmpp_password_override", userpass.password);
+
                                         document.getElementById("loader").style.display = "none";
                                         converse.initialize(config);
 
@@ -157,3 +165,126 @@ window.addEventListener("load", function()
         converse.initialize(config);
     }
 });
+
+function stopRecorder()
+{
+    console.log("stopRecorder");
+
+    if (of.audioRecorder) of.audioRecorder.stop();
+    if (of.videoRecorder) of.videoRecorder.stop();
+}
+
+function startRecorder(recordAudio, recordVideo, localAudioStream, localVideoStream, room, nickname, username, password)
+{
+    console.log("startRecorder", nickname, room, username, localAudioStream, localVideoStream);
+
+    if (recordAudio || recordVideo)
+    {
+        of.audioRecorder = new MediaRecorder(localAudioStream);
+        of.audioChunks = [];
+
+        of.audioRecorder.ondataavailable = function(e)
+        {
+            if (e.data.size > 0)
+            {
+                console.log("startRecorder push audio ", e.data);
+                of.audioChunks.push(e.data);
+            }
+        }
+
+        of.audioRecorder.onstop = function(e)
+        {
+            console.log("audioRecorder.onstop ", e.data);
+            var audioReader = new FileReader();
+
+            audioReader.onload = function()
+            {
+                var file = dataUrlToFile(audioReader.result, room + "." + nickname + ".audio.webm");
+                console.log("audioReader.onload", file);
+                uploadFile(file, room, username, password);
+            };
+
+            audioReader.readAsDataURL(new Blob(of.audioChunks, {type: 'video/webm'}));
+        }
+
+        of.audioRecorder.start();
+    }
+
+    if (recordVideo)
+    {
+        of.videoRecorder = new MediaRecorder(localVideoStream);
+        of.videoChunks = [];
+
+        of.videoRecorder.ondataavailable = function(e)
+        {
+            if (e.data.size > 0)
+            {
+                console.log("startRecorder push video ", e.data);
+                of.videoChunks.push(e.data);
+            }
+        }
+
+        of.videoRecorder.onstop = function(e)
+        {
+            console.log("videoRecorder.onstop ", e.data);
+            var videoReader = new FileReader();
+
+            videoReader.onload = function()
+            {
+                var file = dataUrlToFile(videoReader.result, room + "." + nickname + ".video.webm");
+                console.log("videoReader.onload", file);
+                uploadFile(file, room, username, password);
+            };
+
+            videoReader.readAsDataURL(new Blob(of.videoChunks, {type: 'video/webm'}));
+        }
+
+        of.videoRecorder.start();
+    }
+}
+
+function uploadFile(file, room, username, password)
+{
+    console.log("uploadFile", file, room);
+
+    var server = config.bosh_service_url.split("/")[2];
+
+    var putUrl = "https://" + server + "/dashboard/upload?name=" + file.name + "&username=" + username;
+
+    var req = new XMLHttpRequest();
+
+    req.onreadystatechange = function()
+    {
+      if (this.readyState == 4 && this.status >= 200 && this.status < 400)
+      {
+        console.log("uploadFile", this.statusText);
+      }
+      else
+
+      if (this.readyState == 4 && this.status >= 400)
+      {
+        console.error("uploadFile", this.statusText);
+      }
+
+    };
+    req.open("PUT", putUrl, true);
+    req.setRequestHeader("Authorization", 'Basic ' + btoa(username + ':' + password));
+    req.send(file);
+}
+
+function dataUrlToFile(dataUrl, name)
+{
+    //console.log("dataUrlToFile", dataUrl, name);
+
+    var binary = atob(dataUrl.split(',')[1]),
+    data = [];
+
+    for (var i = 0; i < binary.length; i++)
+    {
+        data.push(binary.charCodeAt(i));
+    }
+
+    return new File([new Uint8Array(data)], name, {type: 'video/webm'});
+}
+
+
